1b386b3403bc2ec8400206efb45f87953bbedefc
RuKey-Sys <kukuevruslan@gmail.com>
2021-03-10 19:22:16 +0300

    Add comments

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ login_manager = LoginManager(app)
 app.secret_key = "#safefsegwe"
 login_manager.login_view = "login"
 
-
+#Описываем таблицу где будут ФИО,Должность и время создания
 class Article(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100), nullable=False)
@@ -24,17 +24,17 @@ class Article(db.Model):
     def __repr__(self):
         return '<Article %r>' % self.id
 
-
+#Описываем таблицу где будет записано оборудование
 class Equipment(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    eq = db.Column(db.String(100), nullable=False)
-    col = db.Column(db.Integer, nullable=False)
-    user_id = db.Column(db.Integer, db.ForeignKey('article.id'))
+    id = db.Column(db.Integer, primary_key=True)    #id
+    eq = db.Column(db.String(100), nullable=False)  #Наименование
+    col = db.Column(db.Integer, nullable=False)     #Количество
+    user_id = db.Column(db.Integer, db.ForeignKey('article.id'))    #Внешний ключ на таблицу с пользователями
 
     def __repr__(self):
         return '<Equipment %r>' % self.id
 
-
+#Описываем таблицу, в которой будут пользователи, которым разрешено посещение сайта
 class Users(db.Model,UserMixin):
     id = db.Column(db.Integer(), primary_key=True)
     login = db.Column(db.String(100), nullable=False)
@@ -55,80 +55,79 @@ def load_user(user_id):
 
 
 
-@app.route('/login/', methods=['post', 'get'])
+@app.route('/login/', methods=['post', 'get'])      #Обработчик ссылки /login
 def login():
-    password = request.form.get('password')
+    password = request.form.get('password')         #в password записываем написанное в поле password на странице
     form = LoginForm()
-    if form.validate_on_submit():
-        user = db.session.query(Users).filter(Users.login == form.username.data).first()
-        if user and user.password == password:
-            login_user(user, remember=form.remember.data)
+    if form.validate_on_submit():                   #Если нажали submit
+        user = db.session.query(Users).filter(Users.login == form.username.data).first()    #Из таблицы Users берем первого пользователя и проверяем с написанным
+        if user and user.password == password:          #Проверяем написанный на сайте пароль, и пароль из таблицы
+            login_user(user, remember=form.remember.data) #пропускаем если все сошлось
             return redirect('/')
         else:
             return redirect(url_for('login'))
-    return render_template('login.html', form=form)
+    return render_template('login.html', form=form)     #При Переходе на /login генерируем страницу login.html
 
 
 @app.route('/')
 @app.route('/home')
-@app.route('/inventory')
+@app.route('/inventory')        #Обработчик ссылок /, /home, /inventory
 @login_required
 def inventory():
 
-    q = request.args.get('q')
+    q = request.args.get('q')       #Аргументы из поля поиска записываем переменную q
     if q:
-        articles = Article.query.filter(Article.title.contains(q)).all()
-        print(articles)
+        articles = Article.query.filter(Article.title.contains(q)).all()    # В переменную article записываем записи, где в поле title содержится q
     else:
-        articles = Article.query.order_by(Article.title).all()
-    return render_template("inventory.html", articles=articles)
+        articles = Article.query.order_by(Article.title).all()      #Иначе показываем всех
+    return render_template("inventory.html", articles=articles)     #При Переходе на ссылки /,/home/inventory генерируем страницу inventory.html
 
 
-@app.route('/inventory/<int:id>')
+@app.route('/inventory/<int:id>')       #Обработчик ссылки /inventory/{id пользователя} "ака Детальная страница пользователя"
 @login_required
 def equip(id):
-    article = Article.query.get(id)
-    equipment = Equipment.query.all()
-    return render_template("eq.html", article=int(article.id), equipment=equipment, name=article)
+    article = Article.query.get(id)     #В article записываем id title и intro пользователя
+    equipment = Equipment.query.all()   #В equipment записываем всю таблицу с оборудованием
+    return render_template("eq.html", article=int(article.id), equipment=equipment, name=article)   #Генерируем страницу eq.html, куда передаем необходимые переменные
 
 
-@app.route('/inventory/<int:id>/delete')
+@app.route('/inventory/<int:id>/delete')    #Удаление пользователя. id-номер пользователя
 @login_required
 def inventory_delete(id):
-    article = Article.query.get_or_404(id)
+    article = Article.query.get_or_404(id)  #Берем пользователя
     equipment = Equipment.query.all()
     for el in equipment:
-        if el.user_id == article.id:
+        if el.user_id == article.id:        #Если оборудование принадлежит пользователю, то удаляем оборудование из таблицы Equipment
             try:
                 db.session.delete(el)
                 db.session.commit()
             except:
                 return 'Error'
     try:
-        db.session.delete(article)
+        db.session.delete(article)          #Удаляем пользователя из таблицы Article
         db.session.commit()
-        return redirect('/inventory')
+        return redirect('/inventory')       #Переходим на страницу со всеми пользователями
     except:
         return 'Error'
 
 
-@app.route('/inventory/<int:id>/<int:eq_id>/deleteeq')
+@app.route('/inventory/<int:id>/<int:eq_id>/deleteeq')      #Удаление отдельного оборудования у пользователя. id-номер пользователя, eq_id- номер оборудования
 @login_required
 def eq_delete(id, eq_id):
-    article = Article.query.get(id)
-    equipment = Equipment.query.all()
+    article = Article.query.get(id)                         #Берем пользователя
+    equipment = Equipment.query.all()                       #Берем все оборудование
     for el in equipment:
-        if article.id == el.user_id:
+        if article.id == el.user_id:                        #Если оборудование принадлежит пользователю, то удаляем оборудование из таблицы Equipment
             if el.id == eq_id:
                 try:
                     db.session.delete(el)
                     db.session.commit()
-                    return redirect('/inventory/' + str(id))
+                    return redirect('/inventory/' + str(id))#Перенапрявляем на детальную страницу пользователя
                 except:
                     return 'Error'
     return redirect('/inventory/' + str(id))
 
-
+#неиспользуемая функция, можно удалить
 @app.route('/inventory/<int:id>/<int:eq_id>/updateeq', methods=['POST', 'GET'])
 @login_required
 def eq_update(id, eq_id):
@@ -147,23 +146,23 @@ def eq_update(id, eq_id):
                     return 'Error'
     return redirect('/inventory/' + str(id))
 
-
+#Редактировать оборудование
 @app.route('/inventory/<int:id>/<int:eq_id>/show', methods=['POST', 'GET'])
 @login_required
 def eq_show(id, eq_id):
     if request.method == 'POST':
-        eq=request.form[f'eq.{eq_id}']
-        col=request.form[f'col.{eq_id}']
-        equipment = Equipment(eq=eq, col=col, user_id=id)
+        eq=request.form[f'eq.{eq_id}']          #Считываем наименование
+        col=request.form[f'col.{eq_id}']        #Считываем количество
+        equipment = Equipment(eq=eq, col=col, user_id=id)       #Записываем что equipment=записи в таблице Equipment с названием eq и количеством col; номер = id
         try:
-            db.session.add(equipment)
+            db.session.add(equipment)       #Добавляем строку в таблицу Equipment
             db.session.commit()
-            eq_delete(id,eq_id)
+            eq_delete(id,eq_id)             #Функция удаления(Удаляем предыдущее значение)
         except:
             return 'Error'
     return redirect('/inventory/' + str(id))
 
-
+#Неиспользуемая функция
 @app.route('/inventory/<int:id>/update', methods=['POST', 'GET'])
 @login_required
 def updateInventory(id):
@@ -186,24 +185,24 @@ def updateInventory(id):
 
         return render_template("updateInventory.html", article=article, equipment=equipment)
 
-
+#Добавление нового оборудования commit_eq-номер последнего оборудования
 @app.route('/inventory/<int:id>/<int:eq_id>/new/<int:commit_eq>', methods=['POST', 'GET'])
 @login_required
 def add_eq(id, eq_id, commit_eq):
     if request.method == 'POST':
-        new_eq = Equipment.query.get(commit_eq)
+        new_eq = Equipment.query.get(commit_eq)     #Берем последнюю запись
 
-        eq = request.form['eq_id']
-        col = request.form['col_id']
-        new_eq = Equipment(eq=eq, col=col, user_id=id)
+        eq = request.form['eq_id']                  #Наименование
+        col = request.form['col_id']                #Количество
+        new_eq = Equipment(eq=eq, col=col, user_id=id)  #Представляем как строка в таблице с необходимыми записями
         try:
-            db.session.add(new_eq)
+            db.session.add(new_eq)                  #Добавляем строку
             db.session.commit()
             return redirect(f'/inventory/{id}')
         except:
-            db.session.rollback()
+            db.session.rollback()                   #В случае ошибки откатываем изменения
             return 'Error'
-    else:
+    else:                                           #До сюда не доходит
         equipment = Equipment.query.get(commit_eq)
         try:
             db.session.add(equipment)
@@ -213,53 +212,49 @@ def add_eq(id, eq_id, commit_eq):
             db.session.rollback()
             return 'Error'
 
-
+#Создание пользователя
 @app.route('/create-article', methods=['POST', 'GET'])
 @login_required
 def createArticle():
-    article = Article.query.all()
-    print(article)
-    if article != []:
-        id = article[-1].id + 1
-    else:
+    article = Article.query.all()       #Берем всех пользователей
+    if article != []:                   #Если список не пустой
+        id = article[-1].id + 1         #id - номер последнего +1
+    else:                               #Иначе id - 1
         id = 1
     title = ''
     intro = ''
-    article = Article(id=id, title=title, intro=intro)
-    equipment = Equipment.query.all()
-    if equipment != []:
-        eq_id = equipment[-1].id + 1
-    else:
+    article = Article(id=id, title=title, intro=intro)  #Представляем что article пустая строка в таблице Article
+    equipment = Equipment.query.all()   #Берем всё оборудование
+    if equipment != []:                 #Если список не пустой
+        eq_id = equipment[-1].id + 1    #eq_id - номер последнего оборудования +1
+    else:                               #Иначе eq_id - 1
         eq_id = 1
-    equipment = Equipment(id=eq_id)
+    equipment = Equipment(id=eq_id)     #Представляем что equipment пустая строка в таблице Equipment, eq_id - номер последнего оборудования
     if request.method == 'POST':
-        try:
-            return render_template("create-article.html", article=article, equipment=equipment)
-        except:
-            return 'Error'
-    else:
-        return render_template("create-article.html")
+        return render_template("create-article.html", article=article, equipment=equipment)
+    '''
+        Генерируем страницу create-article.html, передаем пустую строку с id последнего+1 и eq_id - номер последнего оборудования +1'''
 
-
+#Добавление оборудования у нового пользователя
 @app.route('/inventory/<int:id>/<int:eq_id>/newuser/<int:commit_eq>', methods=['POST', 'GET'])
 @login_required
 def add_user_eq(id, eq_id, commit_eq):
-    title = request.form['Title']
-    intro = request.form['Intro']
+    title = request.form['Title']       #ФИО пользователя
+    intro = request.form['Intro']       #Должность
     if request.method == 'POST':
-        eq = request.form['eq_id']
-        col = request.form['col_id']
+        eq = request.form['eq_id']      #наименование оборудования
+        col = request.form['col_id']    #Количество
         try:
-            newarticle = Article(title=title, intro=intro)
+            newarticle = Article(title=title, intro=intro)  #Записываем нового пользователя в таблицу Article
             db.session.add(newarticle)
-            new_eq = Equipment(eq=eq, col=col, user_id=id)
+            new_eq = Equipment(eq=eq, col=col, user_id=id)  #Записываем оборудование для данного пользователя
             db.session.add(new_eq)
             db.session.commit()
-            return redirect(f'/inventory/{id}')
+            return redirect(f'/inventory/{id}')             #переходим на страницу данного пользователя
         except:
             db.session.rollback()
             return 'Error'
-    else:
+    else:                              #до сюда не доходит
         equipment = Equipment.query.get(commit_eq)
         try:
             db.session.add(equipment)
